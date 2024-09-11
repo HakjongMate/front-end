131304bcfb7344c963a4856f3764b2c96db75e94
devdaradara <devdaradara@gmail.com>
2024-09-11 11:25:32 +0900

    feat: 푸터 구현 #1

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Footer from './components/common/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
