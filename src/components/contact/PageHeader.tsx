33e38c59038c9cb5e8db4df3b5f9eeebbf1e6ebe
devdaradara <devdaradara@gmail.com>
2024-09-17 17:37:46 +0900

    feat: 문의 상담 신청 헤더 컴포넌트 구현 #7

--- src/components/contact/PageHeader.tsx
+++ src/components/contact/PageHeader.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-function PageHeader() {
+const TitleWrapper = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #333;
+  padding: 5px;
+`;
+
+const SubtitleWrapper = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+  margin-bottom: 30px;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const PageHeader: React.FC = () => {
   return (
-    <div>PageHeader</div>
-  )
+    <>
+      <TitleWrapper>문의 및 상담 신청</TitleWrapper>
+      <SubtitleWrapper>
+        문의 내용 및 상담 신청 내용을 남겨주시면,
+        <br />
+        순차적으로 카카오톡 메시지가 전달됩니다.
+      </SubtitleWrapper>
+    </>
+  );
 }
 
-export default PageHeader
+export default PageHeader;
