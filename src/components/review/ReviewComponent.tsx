25aeb6bb9c313380e974ec30afc4cf853e5efacd
devdaradara <devdaradara@gmail.com>
2024-09-17 11:43:26 +0900

    feat: 리뷰 섹션 리스트형 구현 #7

---
+++ src/components/review/ReviewComponent.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Review {
+  id: number;
+  rating: number;
+  type: string;
+  image: string;
+  author: string;
+  date: string;
+  content: string;
+}
+
+interface ReviewComponentProps {
+  review: Review;
+}
+
+const ReviewContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 800px;
+  background-color: #f6f7fc;
+  padding: 30px;
+  margin: 15px 100px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 5px;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 230px;
+`;
+
+const Author = styled.div`
+  font-size: 12px;
+  font-weight: 300;
+`;
+
+const Date = styled.div`
+  font-size: 12px;
+  font-weight: 300;
+`;
+
+const Rating = styled.div`
+  color: #202594;
+  font-size: 18px;
+  margin-bottom: 15px;
+`;
+
+const Content = styled.div`
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 1.5;
+  text-align: justify;
+`;
+
+const Type = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  color: #202594;
+  margin-bottom: 15px;
+`;
+
+const ReviewComponent: React.FC<ReviewComponentProps> = ({ review }) => {
+  return (
+    <ReviewContainer>
+      <LeftContainer>
+        <Type>
+          {review.type === "analyze"
+            ? "생기부 컨설팅"
+            : review.type === "book"
+            ? "책 관련 분석"
+            : "AI 진단"}
+        </Type>
+        <Rating>{"★ ".repeat(review.rating)}</Rating>
+        <Content>{review.content}</Content>
+      </LeftContainer>
+      <RightContainer>
+        <Date>{review.date}</Date>
+        <Author>{review.author}</Author>
+      </RightContainer>
+    </ReviewContainer>
+  );
+};
+
+export default ReviewComponent;
--- src/components/review/ReviewSection.tsx
+++ src/components/review/ReviewSection.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import ReviewComponent from "./ReviewComponent";
+import ReviewJson from "../../assets/data/review.json";
 
-function ReviewSection() {
-  return (
-    <div>ReviewSection</div>
-  )
+interface Review {
+  id: number;
+  rating: number;
+  type: string;
+  image: string;
+  author: string;
+  date: string;
+  content: string;
 }
 
-export default ReviewSection
+const ReviewSectionContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 0;
+  background-color: #fff;
+`;
+
+const ReviewList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  max-width: 1200px;
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 32px;
+  font-weight: 700;
+  color: #000;
+  margin-bottom: 30px;
+  text-align: center;
+`;
+
+const ReviewSection: React.FC = () => {
+  const reviews: Review[] = ReviewJson;
+
+  return (
+    <ReviewSectionContainer>
+      <SectionTitle>서비스 별 더 많은 후기를 만나보세요</SectionTitle>
+      <ReviewList>
+        {reviews.map((review) => (
+          <ReviewComponent key={review.id} review={review} />
+        ))}
+      </ReviewList>
+    </ReviewSectionContainer>
+  );
+};
+
+export default ReviewSection;
