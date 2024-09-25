4992fba4c2ba5b870e81928cd510073ae25db954
devdaradara <devdaradara@gmail.com>
2024-09-25 13:40:05 +0900

    feat: 마이 카트 페이지 구현 #19

--- src/pages/MyPages/MyCartPage.tsx
+++ src/pages/MyPages/MyCartPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import MyCartSection from '../../components/my/MyCartSection';
 
-function MyCartPage() {
+const PageWrapper = styled.div`
+  padding: 40px;
+  max-width: 1080px;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const MyCartPage: React.FC = () => {
   return (
-    <div>MyCartPage</div>
-  )
-}
+    <PageWrapper>
+      <MyCartSection />
+    </PageWrapper>
+  );
+};
 
-export default MyCartPage
+export default MyCartPage;
