e18584c393ab4a365ba8b354cb62a83e25a050c9
devdaradara <devdaradara@gmail.com>
2024-09-25 13:40:58 +0900

    style: 카트 페이지 최소 높이 지정 #19

--- src/pages/MyPages/MyCartPage.tsx
+++ src/pages/MyPages/MyCartPage.tsx
@@ -5,10 +5,10 @@ import MyCartSection from '../../components/my/MyCartSection';
 const PageWrapper = styled.div`
   padding: 40px;
   max-width: 1080px;
+  min-height: 70vh;
   margin: 0 auto;
   background-color: #fff;
   border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const MyCartPage: React.FC = () => {
