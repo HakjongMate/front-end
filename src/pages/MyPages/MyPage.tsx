f072306504000534137971ec97a4b425839ddd2a
devdaradara <devdaradara@gmail.com>
2024-10-01 09:56:34 +0900

    feat: 마이페이지에서 장바구니 제거 #25

--- src/pages/MyPages/MyPage.tsx
+++ src/pages/MyPages/MyPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import MyProfileSection from '../../components/my/MyProfileSection'
 import MyExplorationSection from '../../components/my/MyExplorationSection'
-import MyCartSection from '../../components/my/MyCartSection'
 import MyPurchaseSection from '../../components/my/MyPurchaseSection'
 
 function MyPage() {
@@ -9,7 +8,6 @@ function MyPage() {
     <div>
       <MyProfileSection />
       <MyExplorationSection />
-      <MyCartSection />
       <MyPurchaseSection />
     </div>
   )
