0554f5f9e3cdb902f78e28b7650dd1cfad588f02
devdaradara <devdaradara@gmail.com>
2024-11-12 15:36:47 +0900

    chore: 변경된 서비스 데이터 구성에 맞게 수정 #45

--- src/pages/ServiceAIDetailPage.tsx
+++ src/pages/ServiceAIDetailPage.tsx
@@ -19,8 +19,7 @@ const ServiceAIDetailPage: React.FC = () => {
   // AI 서비스는 세 번째 상품으로 선택
   const product = serviceData[2];
   // 할인된 가격 계산
-  const price = Number(product.price.replace(/,/g, ""));
-  const discountedPrice = (price * (1 - product.discout)).toLocaleString();
+  const discountedPrice = (product.price * (1 - product.discount)).toLocaleString();
 
   const productInfo = {
     ...product,
